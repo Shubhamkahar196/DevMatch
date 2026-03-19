14a3507c902b9e8a14f66ff51e8c5d6eac3e6de9
Shubham kahar <Shubhamkahar196@gmail.com>
2026-03-19 19:59:46 +0530

    added updated profile controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import UserModel from "../models/user.model.js";
 import express from 'express';
 
 
-
+// get profile
 export const getProfile = async (req,res)=>{
    try {
     if(!req.user){
@@ -25,3 +25,62 @@ export const getProfile = async (req,res)=>{
     })
    }
 }
+
+// edit profile
+export const editProfile = async(req,res)=>{
+    try {
+        if(!req.user){
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorize user"
+            })
+        }
+
+        const allowedFields = [
+            "firstName",
+            "lastName",
+            "age",
+            "phoneNumber",
+            "gender",
+            "password"
+        ]
+
+        const updateData = {};
+        // sirf wahi fields jo user ne bheji
+        allowedFields.forEach((field)=>{
+            if(req.body[field] !== undefined){
+                updateData[field] = req.body[field];
+            }
+        })
+
+        // password hash if password want to change the user
+    if(updateData.password){
+        const hashedPassword = await bcrypt.hash(updateData.password,10);
+        updateData.password = hashedPassword;
+    }
+
+    const updatedProfile =  await UserModel.findByIdAndUpdate(
+        req.user._id,
+        updateData,
+        {new: true}
+    ).select("-password");
+
+    res.status(200).json({
+        success: false,
+        message: "Updated profile successfully",
+        user: updatedProfile
+    })
+    } catch (error) {
+         console.log("Error updating profile:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error"
+    });
+    }
+}
+
+
+
+
+
+
