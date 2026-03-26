2e5cb29d336944c5e012fad16a22f61063689341
Shubham kahar <Shubhamkahar196@gmail.com>
2026-03-26 21:57:25 +0530

    added footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,21 @@
-import { BrowserRouter as Router, Routes,Route } from "react-router-dom"
-import Login from "./components/Login"
-import Footer from "./components/Footer"
-import Body from "./components/Body"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Body from "./components/Body"; 
+import Login from "./components/Login";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-   <>
-   <Router basename="/">
-    <Routes >
-      <Route >
-        <Route path="/" element={<Body/>} />
-       <Route path="/login" element={<Login/>}/>
-       <Route path="/footer" element={<Footer/>}/>
-      </Route>
-    </Routes>
-   </Router>
-   </>
-  )
+    <Router>
+      <Routes>
+        
+        <Route path="/" element={<Body />}>
+         
+          <Route path="login" element={<Login />} />
+          <Route path="footer" element={<Footer />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
