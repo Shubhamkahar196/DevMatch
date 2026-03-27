b60d3b3f0567a64b219839f3ff3b474f69dee177
Shubham kahar <Shubhamkahar196@gmail.com>
2026-03-27 09:27:41 +0530

    added login page

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,9 +1,35 @@
-
+import axios from "axios";
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("")
 
-export default Login
+  const handleLogin = async()=>{
+   try {
+    const res = await axios.post("http://localhost:8000/auth/login",
+      {email,password},
+    {withCredentials:true})
+    console.log(res.data);
+   } catch (error) {
+    console.log(error)
+   }
+  }
+  return (
+    <div className="flex items-center justify-center py-6">
+      <fieldset className="fieldset bg-base-300 border-base-300 rounded-box w-xs border p-5">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} className="input" placeholder="Email" />
+
+        <label className="label">Password</label>
+        <input type="password" value ={password} onChange={(e)=>setPassword(e.target.value)} className="input" placeholder="Password" />
+
+        <button className="btn btn-neutral mt-4" onClick={handleLogin}>Login</button>
+      </fieldset>
+    </div>
+  );
+};
+
+export default Login;
