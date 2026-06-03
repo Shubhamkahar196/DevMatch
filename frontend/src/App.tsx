88355a343b56088d816221a708cb0df85e489a79
Shubham kahar <Shubhamkahar196@gmail.com>
2026-06-03 08:52:56 +0530

    improved

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,36 @@
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Body from "./components/Body";
+// import Login from "./components/Login";
+// import Footer from "./components/Footer";
+// import { Provider } from "react-redux";
+// import store from "./utils/Store";
+// import Feed from "./components/Feed";
+// import Profile from "./components/Profile";
+// import Connections from "./components/Connections";
+// import Requests from "./components/Requests";
+
+// function App() {
+//   return (
+//     <Provider store={store}>
+//       <Router basename="/">
+//         <Routes>
+//           <Route path="/" element={<Body />}>
+//             <Route path="/" element={<Feed />} />
+//             <Route path="/login" element={<Login />} />
+//             <Route path="/profile" element={<Profile />} />
+//             <Route path="/connections" element={<Connections />} />
+//             <Route path="/requests" element={<Requests />} />
+//             <Route path="/footer" element={<Footer />} />
+//           </Route>
+//         </Routes>
+//       </Router>
+//     </Provider>
+//   );
+// }
+
+// export default App;
+
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Body from "./components/Body";
 import Login from "./components/Login";
@@ -12,15 +45,15 @@ import Requests from "./components/Requests";
 function App() {
   return (
     <Provider store={store}>
-      <Router basename="/">
+      <Router>
         <Routes>
           <Route path="/" element={<Body />}>
-            <Route path="/" element={<Feed />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/connections" element={<Connections />} />
-            <Route path="/requests" element={<Requests />} />
-            <Route path="/footer" element={<Footer />} />
+            <Route index element={<Feed />} />
+            <Route path="login" element={<Login />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="connections" element={<Connections />} />
+            <Route path="requests" element={<Requests />} />
+            <Route path="footer" element={<Footer />} />
           </Route>
         </Routes>
       </Router>
