46642eee3394ba4796aafc1567c77b01e266262a
Shubham kahar <Shubhamkahar196@gmail.com>
2026-06-04 12:51:15 +0530

    added sesClient

---
+++ backend/utils/sesClient.ts
@@ -0,0 +1,12 @@
+import { SESClient } from "@aws-sdk/client-ses";
+// Set the AWS Region.
+const REGION = "us-east-1";
+// Credentials are automatically resolved using the AWS SDK credential provider chain.
+// For more information, see https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/setting-credentials-node.html
+// Create SES service object.
+const sesClient = new SESClient({ region: REGION ,credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET_KEY!
+}});
+export { sesClient };
+// snippet-end:[ses.JavaScript.createclientv3]
