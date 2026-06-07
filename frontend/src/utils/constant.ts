13082557038d907417ad02f61ccc10192d4f1071
Shubham kahar <Shubhamkahar196@gmail.com>
2026-06-07 18:32:45 +0530

    making env dynamic

--- frontend/src/utils/constant.ts
+++ frontend/src/utils/constant.ts
@@ -1,4 +1,4 @@
 // const BASE_URL = "http://localhost:8000"
-const BASE_URL = "/api";
-
+// const BASE_URL = "/api";
+const BASE_URL = location.hostname === "localhost" ? "http://localhost:8000" : "/api";
 export default BASE_URL
